4c62979956eb6d0620aa210aec196f2878134ada
kenil-kanani <kenilkananigecbvn@gmail.com>
2024-06-29 19:51:07 +0530

    fix: homepage ui

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ import { Card } from "../components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "../components/ui/avatar";
 import { Separator } from "../components/ui/separator";
 import dog from "../assets/br_logo.png";
-import dog2 from "../assets/br_logo.png";
+import dog2 from "../assets/png1.png";
 
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
@@ -18,9 +18,9 @@ import { Link } from "react-router-dom";
 // Dummy Link component to replace Next.js Link
 
 const Hero = () => (
-  <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+  <section className="w-full py-12 md:py-24 lg:py-32 xl:py-20">
     <div className="container px-4 md:px-6">
-      <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+      <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px] object-fill">
         <img
           src={dog}
           alt="Hero"
