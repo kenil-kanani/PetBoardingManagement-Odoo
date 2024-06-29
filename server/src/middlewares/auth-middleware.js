e0e24f3f880f41594303de4719d7e2f8d6853bc1
kenil-kanani <kenilkananigecbvn@gmail.com>
2024-06-29 10:48:00 +0530

    implemented verifyJWT middleware

---
+++ server/src/middlewares/auth-middleware.js
@@ -0,0 +1,29 @@
+
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from 'jsonwebtoken';
+import { User } from "../models/userModel.js";
+import { TOKEN_SECRET } from "../config/serverConfig.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    try {
+        const token = req.header("Authorization")
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+
+        const decodedToken = jwt.verify(token, TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken?._id).select("-password -posts -channels")
+
+        if (!user) {
+            throw new ApiError(401, "Invalid access token")
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+});
